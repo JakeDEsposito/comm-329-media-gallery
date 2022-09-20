21cea188d29fff988cc0b7ebc4cf5d2c3beda168
Jake D'Esposito <jdesposito64@gmail.com>
2022-09-19 21:33:52 -0400

    Added onclick events

--- res/index.js
+++ res/index.js
@@ -1,30 +1,52 @@
-$.getJSON("./res/data.json", (data) => {
-    $.each(data, (i, value) => {
+//$.getJSON("./res/data.json", (data) => {
+//})
 
-        // Media Cards
-        $("#media-cards").append(`
-            <div class="media-card tall">
-                <header>
-                    <img src="${value.url}">
-                </header>
-                <main>
-                    <h1>${value.title}</h1>
-                    <p>${value.description}</p>
-                    <button class="hollow" onclick="displayImg('${value.url}')">Click</button>
-                </main>
-            </div>
-        `)
+const data = dataGenerator(16)
 
-        // Images
-        $("#images").append(`
-            <img src="${value.url}">
-        `)
-    })
+$.each(data, (i, value) => {
 
-    // Current Image
-    $("#current-img").attr("src", data[0].url)
+    // Media Cards
+    $("#media-cards").append(`
+        <div class="media-card tall">
+            <header>
+                <img src="${value.url}">
+            </header>
+            <main>
+                <h1>${value.title}</h1>
+                <p>${value.description}</p>
+                <button class="hollow" onclick="displayImg('${i}')">Click</button>
+            </main>
+        </div>
+    `)
+
+    // Images
+    $("#images").append(`
+        <img src="${value.url}" onclick="displayImg('${i}')">
+    `)
 })
 
-function displayImg (url) {
-    $("#current-img").attr("src", url)
+let current_image_index = 0
+function displayImg (index) {
+    $("#current-img").attr("src", data[index].url)
+    $("#current-img-title").text(data[index].title)
+    
+    window.location.href = window.location.href = "#current-img"
+    current_image_index = index
 }
+
+function nextImg () {
+    let next_image_index = current_image_index + 1
+    if (data.length - 1 < next_image_index)
+        displayImg(0)
+    displayImg(next_image_index)
+}
+
+function prevImg () {
+    let prev_image_index = current_image_index - 1
+    if (prev_image_index < 0)
+        displayImg(data.length - 1)
+    displayImg(prev_image_index)
+}
+
+// Set current image on page reload
+displayImg(0)
