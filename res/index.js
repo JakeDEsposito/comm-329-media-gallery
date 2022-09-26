abab71c39bf4c4dd2c321ae28033c01290fb1ec0
JakeDEsposito <97902257+JakeDEsposito@users.noreply.github.com>
2022-09-26 11:15:33 -0400

    Added Icons

--- res/index.js
+++ res/index.js
@@ -21,12 +21,12 @@ $.each(data, (i, value) => {
 
     // Images
     $("#images").append(`
-        <img src="${value.url}" onclick="displayImg(${i})" id="image-${i}">
+        <img src="${value.url}" onclick="displayMainImg(${i})" id="image-${i}">
     `)
 })
 
 let current_image_index = 0
-function displayImg (index) {
+function displayMainImg (index) {
     $("#current-img").attr("src", data[index].url)
     $("#current-img-title").text(data[index].title)
     
@@ -37,17 +37,34 @@ function displayImg (index) {
 function nextImg () {
     let next_image_index = current_image_index + 1
     if (data.length - 1 <= next_image_index)
-        displayImg(0)
-    displayImg(next_image_index)
+        displayMainImg(0)
+    else
+        displayMainImg(next_image_index)
 }
 
 function prevImg () {
     let prev_image_index = current_image_index - 1
     if (prev_image_index < 0)
-        displayImg(data.length - 1)
-    displayImg(prev_image_index)
+        displayMainImg(data.length - 1)
+    else
+        displayMainImg(prev_image_index)
 }
 
 // Set current image on page reload
 $("#current-img").attr("src", data[0].url)
 $("#current-img-title").text(data[0].title)
+
+function displayImg (index) {
+    $("#popup").show()
+    $("#popup-image").attr("src", data[index].url)
+    $("#popup-title").text(data[index].title)
+    $("#popup-description").text(data[index].description)
+}
+
+/*
+Make big image at top smaller at wider screen sizes to help with seeing that there is more on the page
+
+Use scroll index and scroll snap for #images
+
+When viewing images from media card, make thing pop up
+*/
